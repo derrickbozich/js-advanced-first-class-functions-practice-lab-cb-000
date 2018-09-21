c62c0adf70676b5e3bf0b5cea207697d8643a8c3
derrick b <dbozich1@gmail.com>
2018-09-21 20:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file!
 function logDriverNames(drivers){
   const logNames = function (driver){ return driver.name }
-  return drivers.forEach(logNames)
+  let array = drivers.forEach(logNames)
+  debugger
 }
