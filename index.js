6253c981b721509514309e0d19e3b1bca8365bb9
derrick b <dbozich1@gmail.com>
2018-09-21 21:24:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ function logDriversByHometown(drivers, hometown){
 // }
 
 function driversByName(drivers){
-  newArray = drivers;
   const callback = function(driver1, driver2){
     return driver1.name.localeCompare(driver2.name)
   }
-  return newArray.sort(callback);
+  return drivers.slice()..sort(callback);
 }
