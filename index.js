ddeb828b58454e6854a5a347397828708edd9a9e
derrick b <dbozich1@gmail.com>
2018-09-21 21:12:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function logDriversByHometown(drivers, hometown){
   drivers.forEach(callback);
 }
 
-function driversByRevenue(drivers){
-  const callback = function(driver1, driver2){
-    return driver1.revenue - driver2.revenue;
-  }
-  return drivers.sort(callback);
-}
+// function driversByRevenue(drivers){
+//   const callback = function(driver1, driver2){
+//     return driver1.revenue - driver2.revenue;
+//   }
+//   return drivers.sort(callback);
+// }
